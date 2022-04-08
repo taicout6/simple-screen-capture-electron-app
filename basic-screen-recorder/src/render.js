f0e4bfd82a55f18c55ac8074d756c62b3160d72c
taicout6 <taicout6@gmail.com>
2022-04-08 07:30:30 -0300

    Get the available video sources

--- basic-screen-recorder/src/render.js
+++ basic-screen-recorder/src/render.js
@@ -1,1 +1,13 @@
+const { desktopCapturer, remote } = require('electron');
+
+const { Menu } = remote;
+
 const videoSelectBtn = document.getElementById('videoSelectBtn');
+videoSelectBtn.onclick = getVideoSources;
+
+// Get the available video sources
+async function getVideoSources() {
+  const inputSources = await desktopCapturer.getSources({
+    types: ['window', 'screen']
+  });
+}
